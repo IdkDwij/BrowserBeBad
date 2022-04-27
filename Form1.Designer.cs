72e9023b5774aa1183f14e403bda29d8ffd8681d
Dwij <94942567+IdkDwij@users.noreply.github.com>
2022-04-26 21:23:44 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿using System;
+
+namespace BrowserBeBad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.webBrowser3 = new System.Windows.Forms.WebBrowser();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(0, 0);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "<--";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.AutoSize = true;
+            this.button5.Location = new System.Drawing.Point(72, 1);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 1;
+            this.button5.Text = "-->";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(153, 2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(626, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(825, -2);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 3;
+            this.button6.Text = "Search";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // webBrowser3
+            // 
+            this.webBrowser3.Location = new System.Drawing.Point(0, 28);
+            this.webBrowser3.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser3.Name = "webBrowser3";
+            this.webBrowser3.Size = new System.Drawing.Size(900, 411);
+            this.webBrowser3.TabIndex = 4;
+            this.webBrowser3.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser3_DocumentCompleted);
+            this.webBrowser3.Navigated += new System.Windows.Forms.WebBrowserNavigatedEventHandler(this.webBrowser3_Navigated);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(774, -1);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(58, 23);
+            this.button7.TabIndex = 5;
+            this.button7.Text = "Refresh";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(903, 444);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.webBrowser3);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Name = "Form1";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.WebBrowser webBrowser2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.WebBrowser webBrowser3;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BrowserBeBad
+{
+    
+    public partial class Form1 : Form
+    {
+        public int buttonOffset = 20,browserOffset,textOffset;
+        private void Resolution()
+        {
+            int h = Screen.PrimaryScreen.WorkingArea.Height;
+            int w = Screen.PrimaryScreen.WorkingArea.Width;
+            this.ClientSize = new Size(w, h);
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            buttonOffset = this.Width - button4.Width;
+            browserOffset = this.Width - webBrowser3.Width;
+            textOffset = this.Width - textBox2.Width;
+        }
+
+        private void webBrowser3_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            webBrowser3.GoBack();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            webBrowser3.GoForward();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            webBrowser3.Refresh();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            webBrowser3.Navigate(textBox2.Text);
+            webBrowser3.ScriptErrorsSuppressed = false;
+        }
+
+        private void webBrowser3_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+        {
+            textBox2.Text = webBrowser3.Url.ToString();
+        }
+        private void button5_Load(object sender, WebBrowserNavigatedEventArgs e)
+        {
+            Resolution();
+            textBox2.Text = webBrowser3.Url.ToString();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "easteregg.com") webBrowser3.Navigate("https://www.pornhub.com");
+            button4.Width = this.Width - buttonOffset;
+            button5.Width = this.Width - buttonOffset;
+            button6.Width = this.Width - buttonOffset;
+            button7.Width = this.Width - buttonOffset;
+            webBrowser3.Width = this.Width - browserOffset;
+            textBox2.Width = this.Width - textOffset;
+        }
+        private int GetNewButtonWidth()
+        {
+            return this.Width/2 - buttonOffset;
+        }
+        private int GetNewTextWidth()
+        {
+            return this.Width/2 - textOffset;
+        }
+        private int GetNewBrowserWidth()
+        {
+            return this.Width/2 - browserOffset;
+        }
+    }
+}
